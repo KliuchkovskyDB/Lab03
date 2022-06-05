9fa64dac9f7f6c4d7c5151b97a51d27a1fde3114
Daniil <lakvain@gmail.com>
2022-06-05 16:43:13 +0300

    code: input numbers

--- main.cpp
+++ main.cpp
@@ -5,12 +5,12 @@
 
 using namespace std;
 
-vector<double>input_numbers(size_t count)
+vector<double>input_numbers(istream& in, size_t count)
 {
     vector<double> result(count);
     for (size_t i = 0; i < count; i++)
     {
-        cin >> result[i];
+        in >> result[i];
     }
     return result;
 }
@@ -73,7 +73,7 @@ int main()
     cerr << "Enter number count: ";
     cin >> number_count;
     cerr << "Enter numbers: ";
-    const auto numbers = input_numbers(number_count);
+    const auto numbers = input_numbers(cin, number_count);
 
     size_t bin_count;
     cerr << "Enter bin count: \n";
