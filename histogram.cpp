44390cae653b7266c7bf49cdc2bd1e97521a60f7
Daniil <lakvain@gmail.com>
2022-06-05 19:12:31 +0300

    code: Упорядочение передачи

--- histogram.cpp
+++ histogram.cpp
@@ -1,5 +1,10 @@
 #include "histogram.h"
 
+struct Input {
+	vector<double> numbers;
+	size_t bin_count;
+};
+
 void find_minmax(vector<double> numbers, double& min, double& max)
  {
     if (numbers.size() == 0) {
@@ -16,3 +21,20 @@ void find_minmax(vector<double> numbers, double& min, double& max)
         }
     }
  }
+
+vector <size_t>make_histogram(struct Input input)
+{
+    double min, max;
+    find_minmax(input.numbers, min, max);
+    vector <size_t> bins (input.bin_count);
+    for (double number: input.numbers)
+    {
+       size_t bin = (size_t)((number-min)/(max-min)*input.bin_count);
+       if (bin == input.bin_count)
+       {
+           bin--;
+       }
+       bins[bin]++;
+}
+return bins;
+}
